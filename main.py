28d01b3ce4621cefebea0632481fc17834b9d1c7
Micahel-martin <michael.martin@students.fhu.edu>
2024-02-01 14:19:49 -0600

    first part work
    Started doing some soft work

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 def sum_of_products(list1, list2):
-    pass
-
+    math_sum = 0 
+    for i in range(len(list1)):
+        math_sum +=(int(list1[i] * int(list2)))
+        return math_sum
 if __name__ == '__main__':
 
     pass

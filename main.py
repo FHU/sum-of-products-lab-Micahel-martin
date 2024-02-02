b0cb1264e72aac6b6225c8fee493be2083fdadbe
Micahel-martin <michael.martin@students.fhu.edu>
2024-02-02 16:16:05 -0600

    fixed a typo

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 def sum_of_products(list1, list2):
     math_sum = 0 
     for i in range(len(list1)):
-        math_sum +=(int(list1[i] * int(list2)))
+        math_sum +=(int(list1[i]) * int(list2[i]))
         return math_sum
+
 if __name__ == '__main__':
     in1 = (input()).split()
     in2= (input()).split()

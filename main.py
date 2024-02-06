778ae4a92f36aebdbe43d64e978811ccbcb9bb82
Micahel-martin <michael.martin@students.fhu.edu>
2024-02-06 12:12:40 -0600

    reverted changes
    Reverted back 2 saves

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ def sum_of_products(list1, list2):
     for i in range(len(list1)):
         math_sum +=(int(list1[i]) * int(list2[i]))
         return math_sum
-        
+
+if __name__ == '__main__':
     in1 = (input()).split()
     in2= (input()).split()
     if len(in1) == len(in2):
         result= sum_of_products(in1, in2)
         print (result)
     else:
-        print('Error.')
-
-if __name__ == '__main__':
-    pass
+        return ('Error.')

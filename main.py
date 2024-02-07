f392ec6fe1e444f1ead323aaebeb1c0576ec8e86
Micahel-martin <michael.martin@students.fhu.edu>
2024-02-06 21:36:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 def sum_of_products(list1, list2):
+    new=0
     math_sum = 0 
     for i in range(len(list1)):
-        math_sum +=(int(list1[i]) * int(list2[i]))
-        return math_sum
+        math_sum =(int(list1[i])) * int(list2[i])
+        new += math_sum
+
+
+
+
+    return new 
 
 if __name__ == '__main__':
     in1 = (input()).split()
@@ -12,4 +18,4 @@ if __name__ == '__main__':
 
     result= sum_of_products(in1, in2)
     print (result)
-    #fi
+    
